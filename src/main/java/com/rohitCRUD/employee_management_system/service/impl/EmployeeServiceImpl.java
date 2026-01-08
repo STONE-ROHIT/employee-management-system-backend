795904c93300220a2bc10fc6da8df68252918b22
Rohit Kumar <rohitkashyap1207@gmail.com>
2026-01-08 18:52:58 +0530

    Initial commit: Spring Boot CRUD backend with PostgreSQL

---
+++ src/main/java/com/rohitCRUD/employee_management_system/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,64 @@
+package com.rohitCRUD.employee_management_system.service.impl;
+
+import com.rohitCRUD.employee_management_system.dto.EmployeeDto;
+import com.rohitCRUD.employee_management_system.entity.Employee;
+import com.rohitCRUD.employee_management_system.exception.ResourceNotFoundException;
+import com.rohitCRUD.employee_management_system.mapper.EmployeeMapper;
+import com.rohitCRUD.employee_management_system.repository.EmployeeRepository;
+import com.rohitCRUD.employee_management_system.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        Employee savedEmployee = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee does not exists with this id : " + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee)  -> EmployeeMapper.mapToEmployeeDto(employee))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee does not exists with this id : " + employeeId)
+        );
+
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+
+        Employee updatedEmployeeObj = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+    }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee does not exists with this id : " + employeeId)
+        );
+
+        employeeRepository.deleteById(employeeId);
+    }
+}
---
+++ src/test/java/com/rohitCRUD/employee_management_system/EmployeeManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohitCRUD.employee_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
